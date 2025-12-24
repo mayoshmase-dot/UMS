ac1f747ee6e18c7aac79c0065f7ce53edbe01faa
mayoshmase-dot <mayoshmase@gmail.com>
2025-12-24 18:01:36 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const getUsers = async()=> {
+    const response = await axios.get('http://ums12.runasp.net/api/users?limit=500');
+    return response.data;
+}
+const displayUsers = async() => {
+    const result = await getUsers();
+    const users = result.users.map((user) => {
+        return `
+        <tr>
+        <td>${user.name}</td>
+        <td><img src="${user.imageUrl}" width = "100px"/> </td>
+            <td><button class ="btn btn-outline-danger" onclick=deleteUsers(${user.id})>Delete</button></td>
+        </tr>
+        `
+    }).join(' ');
+    console .log(users);
+    document.querySelector(".users .data-body").innerHTML = users;
+}
+displayUsers();
+const deleteUsers =async(id)=>{
+const response = await axios.delete (`http://ums12.runasp.net/api/users/${id}`);
+}
